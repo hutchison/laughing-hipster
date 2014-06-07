974a38b437825a4c5815a503497442590cae1af0
Martin Darmüntzel <git@trivialanalog.de>
2014-06-07 20:58:02 +0200

    Fügt die Installation von Munki hinzu.
    Dabei wird (rudimentär) getestet, ob Munki nicht schon installiert ist.

--- bootstrap.sh
+++ bootstrap.sh
@@ -25,6 +25,45 @@ is_salt_installed() {
     [[ -x '/usr/local/bin/salt' ]]
 }
 
+# Is Munki installed?
+is_munki_installed() {
+    local MUNKIDIR="/usr/local/munki"
+    [[ -d "$MUNKIDIR" ]] \
+        && [[ -x "$MUNKIDIR/managedsoftwareupdate" ]]
+}
+
+# Installs Munki:
+install_munki() {
+    echo "Installing Munki..."
+
+    local DMG_NAME="munkitools-latest.dmg"
+    local DMG_URL="https://munkibuilds.org/$DMG_NAME"
+    local LOCAL_DMG_PATH="$(pwd)/$DMG_NAME"
+
+    local TMPMOUNT=$(/usr/bin/mktemp -d /tmp/munkibuilds.XXXX)
+    local HDIUTIL="/usr/bin/hdiutil"
+    local CURL="/usr/bin/curl"
+
+    echo "Grabbing the latest version..."
+    $CURL -s --connect-timeout 30 -o "$LOCAL_DMG_PATH" "$DMG_URL"
+
+    echo "Mounting at $TMPMOUNT ..."
+    $HDIUTIL attach "$LOCAL_DMG_PATH" -mountpoint "$TMPMOUNT"
+    local MUNKI_MPKG_PATH="$TMPMOUNT/$(/bin/ls $TMPMOUNT | grep munkitools)"
+
+    echo "Installing Munki from $MUNKI_MPKG_PATH ..."
+    sudo /usr/sbin/installer -pkg "$MUNKI_MPKG_PATH" -target /
+
+    echo "Unmounting installer dmg..."
+    $HDIUTIL detach "$TMPMOUNT"
+
+    echo "Cleaning up..."
+    /bin/rm -f "$DMG_NAME"
+    /bin/rm -rf "$TMPMOUNT"
+
+    echo "Done."
+}
+
 main() {
     if ! on_a_mac; then
         echo "We are not on a Mac. Exiting."
@@ -37,6 +76,10 @@ main() {
 
     if ! is_salt_installed; then
         brew install saltstack
+    fi
+
+    if ! is_munki_installed; then
+        install_munki
     fi
 }
 
